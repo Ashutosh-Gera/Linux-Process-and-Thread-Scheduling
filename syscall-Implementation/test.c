250338367b38c9c3268e49bdb16902b4e2108457
Ashutosh-Gera <ashu19.iiitd@gmail.com>
2023-02-05 03:08:52 +0530

    Yes, now all done

--- syscall-Implementation/test.c
+++ syscall-Implementation/test.c
@@ -6,18 +6,18 @@
 
 int main(){
 
-    float mat1[10][10];
-    float mat2[10][10];
-    int row = 10;
-    int col = 10;
-    for(int i = 0;i<5;i++){
-        for(int j = 0;j<5;j++){
+    float mat1[2][2];
+    float mat2[2][2];
+    int row = 2;
+    int col = 2;
+    for(int i = 0;i<2;i++){
+        for(int j = 0;j<2;j++){
             mat1[i][j] = i+j;
         }
     }
     printf("Original matrix:\n");
-    for(int i = 0;i<10;i++){
-        for(int j = 0;j<5;j++){
+    for(int i = 0;i<2;i++){
+        for(int j = 0;j<2;j++){
             printf("%f ",mat1[i][j]);
         }
         printf("\n");
@@ -25,8 +25,8 @@ int main(){
     long x = syscall(SYS_kernel_2d_memcpy,mat1,mat2,row,col);
     printf("System call returned %d.\n",x);
     printf("Copied mat:\n");
-    for(int i = 0;i<10;i++){
-        for(int j = 0;j<10;j++){
+    for(int i = 0;i<2;i++){
+        for(int j = 0;j<2;j++){
             printf("%f ",mat2[i][j]);
         }
         printf("\n");
